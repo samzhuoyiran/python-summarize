818e089e6941214399e17c436ef89ea16811061e
卓怡然 <38436053+samzhuoyiran@users.noreply.github.com>
2018-04-18 17:45:37 +0800

    while循环非阻塞模拟多任务

---
+++ 单线程使用while循环模拟多任务.py
@@ -0,0 +1,116 @@
+import socket
+import re
+
+import time
+
+
+def main():
+    tcp_server = WebServer()
+    tcp_server.run_server()
+
+
+class WebServer(object):
+    def run_server(self):
+        """运行服务"""
+        # 客户端列表
+        clients = list()
+
+        # 设置服务器端非堵塞
+        self.tcp_server.setblocking(False)
+        while True:
+            try:
+                client, addr = self.tcp_server.accept()
+                # 客户端也是非堵塞
+                client.setblocking(False)
+
+                # 放到我们的列表中
+                clients.append(client)
+            except Exception as e:
+                pass
+
+            for client_new in clients:
+
+                try:
+                    data = client_new.recv(1024).decode()
+                    print(data)
+                    if data:
+                        # 有数据
+                        self.client_exec(client_new, data)
+                    else:
+                        print("关闭了客户端")
+                        # 关闭了客户端
+                        client_new.close()
+                        clients.remove(client_new)
+
+                except Exception as e:
+                    pass
+
+        # 关闭服务器
+        self.tcp_server.close()
+
+    def client_exec(self, client, data):
+        """这个就是单独客户端的处理"""
+        # 接收数据
+
+        head_lines = data.splitlines()
+        try:
+            print(head_lines[0])
+            re_match = re.match(r'[^/]+(/[^ ]*)', head_lines[0])
+            # 判断是否匹配了
+            if re_match:  # 匹配 了
+                file_name = re_match.group(1)
+                # 如果是/那么去首页
+                if file_name == "/":
+                    file_name = "/index.html"
+        except Exception as e:
+            print(e)  # 工作中是记录到文件
+
+        # 返回数据
+        # 响应头
+        # 空行
+        # 响应体
+        try:
+            headers = "HTTP/1.1 200 OK\r\n"
+
+            # 会根据不同的地址返回不的内容
+            # 打开文件写读文件内容
+            with open("./html%s" % file_name, 'rb') as f:  # 这样写有一个好处,如果是图片就不会有问题
+                body = f.read()  # 读取文件
+
+            # body = "show page is find!"
+
+            # content = headers +"\r\n" +body
+            content = headers + "\r\n"
+
+            # client.send(content.encode("utf-8"))
+            client.send(content.encode("utf-8"))
+            client.send(body)
+        except Exception as e:
+            print(e)
+            # 返回一个404的正常显示的页面
+            head = "HTTP/1.1 404 NOT FIND\r\n"
+            body = "not find page!"
+            content = head + "\r\n" + body
+
+            client.send(content.encode("utf-8"))
+
+        # 关闭客户端
+        client.close()
+
+    # alt+j列编辑
+    def __init__(self):  # ctrl+B进入到函数
+        """ 初始化tcp服务器"""
+        # 服务器tcp服务器对象
+        self.tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 设置我们的端口地址重用
+        self.tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # 绑定端口号
+        self.tcp_server.bind(("", 6789))
+        # 改成被动模式
+        self.tcp_server.listen(128)
+
+    # return tcp_server
+
+
+if __name__ == '__main__':
+    main()
