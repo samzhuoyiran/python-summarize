bf31a53ede44bc751dcdd62099b1955e9d8c8185
samzhuo <samzhuoyiran@foxmail.com>
2018-06-13 22:41:32 +0800

    isfunny

---
+++ my_test/isfunny.py
@@ -0,0 +1,38 @@
+# def sum_two_smallest_numbers(numbers):
+#     # your code here
+#     print(numbers)
+#     numbers.sort()
+#     a = numbers
+#     print(a)
+#     x = numbers[0] + numbers[1]
+#     print(x)
+#
+#     return x
+#
+#
+# li = [12,2,33,2]
+# print(sum_two_smallest_numbers(li))
+
+#              1
+#           3     5
+#        7     9    11
+#    13    15    17    19
+# 21    23    25    27    29
+# ...
+# 从行索引（从索引1开始）计算此三角形的行和
+
+def ret(n):
+    return n**3
+    # first = 0
+    # for i in range(1,n-1):
+    #     first+=i
+    # print(first)
+    # first=first*2+1
+    # ret = first*n+
+    # print(first)
+    # print(last)
+
+
+print(ret(5))
+
+
